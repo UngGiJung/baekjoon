2ab080d3f2a0b2afa716038af74d5b7f1b51ee6b
Class01 <Class01@DESKTOP-UR65MFL>
2023-05-12 17:18:12 +0900

    05.12

---
+++ baekjoon/src/q별찍기6/Main.java
@@ -0,0 +1,15 @@
+package q별찍기6;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		for (int i = 0; i <= 5; i++ ) {
+			for(int j = 5; j >= i; j--) {
+				System.out.print("*");
+			}
+		}
+	}
+
+}
